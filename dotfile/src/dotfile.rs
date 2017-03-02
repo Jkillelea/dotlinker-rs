0235a332da953a83c6419c494319e7b4961a188e
Jacob Killelea <Jkillelea@protonmail.ch>
2017-03-01 21:48:37 -0700

    formatting

--- dotfile/src/dotfile.rs
+++ dotfile/src/dotfile.rs
@@ -13,17 +13,18 @@ pub struct DotFile {
 
 impl fmt::Debug for DotFile {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        write!(f, "DotFile {{ \n\texists:        {}\
-        \n\tbasename:      {}\
-        \n\tabsolute_path: {}\
-        \n\tdotfile_path:  {}\
-        \n\thomedir:       {}\
-        \n}}",
-                self.exists,
-                self.basename.display(),
-                self.absolute_path.display(),
-                self.dotfile_path.display(),
-                self.homedir.display())
+        write!(f, "DotFile {{ \
+            \n\texists:        {}\
+            \n\tbasename:      {}\
+            \n\tabsolute_path: {}\
+            \n\tdotfile_path:  {}\
+            \n\thomedir:       {}\
+            \n}}",
+            self.exists,
+            self.basename.display(),
+            self.absolute_path.display(),
+            self.dotfile_path.display(),
+            self.homedir.display())
     }
 }
 
@@ -79,13 +80,11 @@ impl DotFile {
 
 fn init(relpath: PathBuf) -> Result<DotFile, &'static str> { // init handles preparing a DotFile struct
     let homedir = match env::home_dir() { // Option<PathBuf>
-        Some(path) => {
-            path
-        },
+        Some(path) => path,
         _ => return Err("Can't find home direcotry!")
     };
 
-    let absolute_path = PathBuf::from(&fs::canonicalize(relpath).expect("Given bogus path!"));
+    let absolute_path = PathBuf::from( &fs::canonicalize(relpath).expect("Given bogus file path!") );
     let basename      = PathBuf::from(absolute_path.file_name().expect("No file name!"));
     let dotfile_path  = PathBuf::new(); // temporary
     let exists        = absolute_path.exists();
